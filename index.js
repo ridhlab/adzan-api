f20cc073ff5934f76926c2468380dfac2cc47c40
Muhammad Ridwan <mohridwan1707@gmail.com>
2023-04-01 13:17:32 +0700

    move index file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const routes = require("./routes");
+const routes = require("./src/routes");
 
 const port = process.env.PORT || 3000;
 const server = express();
