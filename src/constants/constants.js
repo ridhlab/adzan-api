452620441c48bee16dfe35ef9e80a3c5c07460c9
Muhammad Ridwan <mohridwan1707@gmail.com>
2023-04-02 11:37:12 +0700

    update constant

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 const { data: cities } = require("../data/city.json");
 
 module.exports = {
-    TARGET_URL: process.env.TARGET_URL,
+    TARGET_URL: "https://www.jadwalsholat.org/adzan/monthly.php/",
     NUM_OF_CITY: cities.length,
 };
